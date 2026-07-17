b3250a3a2293f00ad91048ae9907c0fdb0363437
jayjoke <65462136+jayjoke12@users.noreply.github.com>
2026-07-17 21:48:18 +0100

    feat: Initialize TNT Optimiser mod with Fabric

---
+++ src/client/java/com/jayjoke/tntoptimised/client/TNTOptimiserClient.java
@@ -0,0 +1,10 @@
+package com.jayjoke.tntoptimised.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TNTOptimiserClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
